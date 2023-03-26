31d49318eb4a1778fdea307e33a8ffe31fa0d56d
Satyam-22747 <128983226+Satyam-22747@users.noreply.github.com>
2023-03-26 20:11:05 +0530

    Update Swapping using temp & Loc var

---
+++ swaplocvar.java
@@ -0,0 +1,16 @@
+class swaplocvar
+{
+    public static void main(String args[])
+{
+    int a=10, b=20, temp;
+    System.out.println("Value of a before swap="+a);
+    System.out.println("Value of b before swap="+b);
+    temp=a;
+    a=b;
+    b=temp;
+    System.out.println("Value of a after swap="+a);
+    System.out.println("Value of b after swap="+b);
+    
+}
+}
+
