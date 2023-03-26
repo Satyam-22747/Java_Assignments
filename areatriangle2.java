1c08d79df2b4d0c379876032381bd4edbd3175f7
Satyam-22747 <128983226+Satyam-22747@users.noreply.github.com>
2023-03-26 20:01:56 +0530

    Area of triangle inst Var

---
+++ areatriangle2.java
@@ -0,0 +1,11 @@
+class areatriangle2
+{
+  int length=12, breadth=5;
+  public static void main(String args[])
+{
+float area;
+areatriangle2 ar=new areatriangle2();
+area=(ar.length)*(ar.breadth)/2;
+System.out.println("Area="+area);
+}
+}
