0f7f1aca36452f424a5e1dd0757fbc95a260fd13
Satyam-22747 <128983226+Satyam-22747@users.noreply.github.com>
2023-03-26 20:05:04 +0530

    Update Pythagorous theorem Loc Variable

---
+++ pythagorous.java
@@ -0,0 +1,9 @@
+class pythagorous
+{
+    public static void main(String args[])
+    {
+        int hy=5,b=3,ht=4;
+        System.out.println("Square of hypotenuse="+(hy*hy));
+        System.out.println("Sum of squares of base and height="+(b*b+ht*ht));
+    }
+}
