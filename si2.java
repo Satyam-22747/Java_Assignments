27d67223d4b838365ad1788191efbae157931e55
Satyam-22747 <128983226+Satyam-22747@users.noreply.github.com>
2023-03-26 20:08:43 +0530

    Update Simple Interest Inst variable

---
+++ si2.java
@@ -0,0 +1,11 @@
+class si2
+{
+    int pr=20000, r=5, t=10;
+    public static  void main(String args[])
+{
+    float si;
+    si2 s=new si2();
+    si=(s.pr*s.r*s.t)/100;
+    System.out.println("Simple interest="+si);
+}
+}
