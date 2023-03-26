b04369b5e7ed8bcc494fd23898a129d1e8f16967
Satyam-22747 <128983226+Satyam-22747@users.noreply.github.com>
2023-03-26 20:05:35 +0530

    Update Pythagorous theorem Inst Variable

---
+++ pythagorous2.java
@@ -0,0 +1,10 @@
+class pythagorous2
+{
+    int hy=13, b=12, ht=5;
+    public static void main(String args[])
+    {
+        pythagorous2 py=new pythagorous2();
+        System.out.println("Square of hypotenuse="+(py.hy*py.hy));
+        System.out.println("Sum of squares of base and height="+(py.b*py.b+py.ht*py.ht));
+    }
+}
