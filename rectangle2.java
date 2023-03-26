a69d53f35fe49f28c7dffa086fb3e5c8b1c801c4
Satyam-22747 <128983226+Satyam-22747@users.noreply.github.com>
2023-03-26 20:07:04 +0530

    Update Area of rectangle Inst Variable

---
+++ rectangle2.java
@@ -0,0 +1,11 @@
+class rectangle2
+{
+  int length=28, breadth=10;
+  public static void main(String args[])
+{
+float area;
+rectangle2 ar=new rectangle2();
+area=(ar.length)*(ar.breadth);
+System.out.println("Area="+area);
+}
+}
